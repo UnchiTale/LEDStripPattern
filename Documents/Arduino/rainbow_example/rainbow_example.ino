2261cc1c8f536c05727fd5598bf33a5a61511aa7
BuildTools <unconfigured@null.spigotmc.org>
2019-07-22 02:20:23 +0900

    発光パターンの関数4種類

---
+++ Documents/Arduino/rainbow_example/rainbow_example.ino
@@ -0,0 +1,65 @@
+#include <Adafruit_NeoPixel.h>
+
+#define MAX_VAL 64
+#define DELAY_TIME 50
+#define DELAY_TIME2 20
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(170, 6, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show();
+}
+
+void loop() {
+
+  rainbow(0);
+}
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  // 5 cycles of all colors on wheel
+  for (j = 0; j < 256 * 5; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+uint32_t Wheel(byte WheelPos) {
+  if (WheelPos < 85) {
+    return strip.Color((WheelPos * 3) * MAX_VAL / 255, (255 - WheelPos * 3) * MAX_VAL / 255, 0);
+  } else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color((255 - WheelPos * 3) * MAX_VAL / 255, 0, (WheelPos * 3) * MAX_VAL / 255);
+  } else {
+    WheelPos -= 170;
+    return strip.Color(0, (WheelPos * 3) * MAX_VAL / 255, (255 - WheelPos * 3) * MAX_VAL / 255);
+  }
+}
